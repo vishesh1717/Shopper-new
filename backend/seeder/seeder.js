9fe03c037f2740bd1b4ccfd070228bb0af6e2164
vish <visheshh22@gmail.COM>
2025-04-20 10:23:22 +0530

    tax rates changes

--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -5,7 +5,7 @@ import Product from "../models/product.js";
 const seedProducts = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://ghulamabbas:ghulamabbas@shopit.wkd8107.mongodb.net/shopit-v2?retryWrites=true&w=majority"
+      "mongodb+srv://<dbdetails>@cluster.cduljku.mongodb.net/shopit-v2?retryWrites=true&w=majority"
     );
 
     await Product.deleteMany();
